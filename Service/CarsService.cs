fd129311f2325b9be38622b80199f088d38de953
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-09-01 09:24:45 -0600

    cars and houses service file completed

--- Service/CarsService.cs
+++ Service/CarsService.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+using gregslist_c_sharp.Models;
+
 namespace gregslist_c_sharp.Service
 {
     public class CarsService
     {
-        
+        internal IEnumerable<Car> Get()
+        {
+            return FakeDb.Cars;
+        }
+
+        internal Car Get(string id)
+        {
+            Car found = FakeDb.Cars.Find(j => j.Id == id);
+            if(found == null)
+            {
+                throw new System.Exception("Invalid Id");
+            }
+            return found;
+        }
+
+        internal Car Create(Car rawCar)
+        {
+            FakeDb.Cars.Add(rawCar);
+            return rawCar;
+        }
+
+        internal void Delete(string id)
+        {
+            int deleted = FakeDb.Cars.RemoveAll(j => j.Id == id);
+            if(deleted == 0)
+            {
+                throw new System.Exception("Invalid Id");
+            }
+            System.Console.WriteLine("Deleted Car");
+
+        }
     }
 }
--- Service/HousesService.cs
+++ Service/HousesService.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+using gregslist_c_sharp.Models;
+
 namespace gregslist_c_sharp.Service
 {
     public class HousesService
     {
-        
+        internal IEnumerable<House> Get()
+        {
+            return FakeDb.Houses;
+        }
+
+        internal House Get(string id)
+        {
+            House found = FakeDb.Houses.Find(j => j.Id == id);
+            if(found == null)
+            {
+                throw new System.Exception("Invalid Id");
+            }
+            return found;
+        }
+
+        internal House Create(House rawHouse)
+        {
+            FakeDb.Houses.Add(rawHouse);
+            return rawHouse;
+        }
+
+        internal void Delete(string id)
+        {
+            int deleted = FakeDb.Houses.RemoveAll(j => j.Id == id);
+            if(deleted == 0)
+            {
+                throw new System.Exception("Invalid Id");
+            }
+            System.Console.WriteLine("Deleted House");
+
+        }
     }
 }
